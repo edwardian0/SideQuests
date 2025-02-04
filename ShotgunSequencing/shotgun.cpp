1dd0e01a652a838ec281777de093612b508754d3
Edward Okoekpen <128243564+edwardian0@users.noreply.github.com>
2025-02-04 11:44:52 +0000

    Add files via upload

---
+++ ShotgunSequencing/shotgun.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "fragments.h"
+#include "overlap.h"
+#include "debug.h"
+#include "shotgun_sequencer.h"
+
+// This function contains our program's core functionality:
+
+void run (std::vector<std::string>& args)
+{
+  debug::verbose = std::erase(args, "-v");
+
+  if (args.size() < 3)
+    throw std::runtime_error ("missing arguments - expected 2 arguments: input_fragments output_sequence");
+
+  auto fragments = load_fragments (args[1]);
+
+  ShotgunSequencer sequencer(fragments);
+  sequencer.init (fragments);
+
+  std::cerr << "initial sequence has size " << sequencer.sequence().size() << "\n";
+
+  while (sequencer.iterate());
+
+  sequencer.check_remaining_fragments();
+
+  std::cerr << "final sequence has length " << sequencer.sequence().size() << "\n";
+
+  write_sequence (args[2], sequencer.sequence());
+}
+
+
+
+
+// skeleton main() function, whose purpose is now to pass the arguments to
+// run() in the expected format, and catch and handle any exceptions that may
+// be thrown.
+
+int main (int argc, char* argv[])
+{
+  try {
+    std::vector<std::string> args (argv, argv+argc);
+    run (args);
+  }
+  catch (std::exception& excp) {
+    std::cerr << "ERROR: " << excp.what() << " - aborting\n";
+    return 1;
+  }
+  catch (...) {
+    std::cerr << "ERROR: unknown exception thrown - aborting\n";
+    return 1;
+  }
+
+  return 0;
+}
